245a2f0b40466cadf65c88c5f46e7d941e50718d
Jeremy Felt <jeremy.felt@gmail.com>
2017-12-30 13:56:09 -0800

    Remove custom header support

--- functions.php
+++ functions.php
@@ -136,11 +136,6 @@ function writemore_scripts() {
 add_action( 'wp_enqueue_scripts', 'writemore_scripts' );
 
 /**
- * Implement the Custom Header feature.
- */
-require get_template_directory() . '/inc/custom-header.php';
-
-/**
  * Custom template tags for this theme.
  */
 require get_template_directory() . '/inc/template-tags.php';
