ee8d092119f890bccc83b1ad8b967a677fbd09f0
Jeremy Felt <jeremy.felt@gmail.com>
2020-01-12 11:54:04 -0800

    Name the entire section "reactions" vs "thoughts"

--- comments.php
+++ comments.php
@@ -31,13 +31,13 @@ if ( post_password_required() ) {
 			if ( 1 === $comment_count ) {
 				printf(
 					/* translators: 1: title. */
-					esc_html_e( 'One thought on &ldquo;%1$s&rdquo;', 'writemore' ),
+					esc_html_e( 'One reaction on &ldquo;%1$s&rdquo;', 'writemore' ),
 					'<span>' . get_the_title() . '</span>'
 				);
 			} else {
 				printf( // WPCS: XSS OK.
 					/* translators: 1: comment count number, 2: title. */
-					esc_html( _nx( '%1$s thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', $comment_count, 'comments title', 'writemore' ) ),
+					esc_html( _nx( '%1$s reactions on &ldquo;%2$s&rdquo;', '%1$s reactions on &ldquo;%2$s&rdquo;', $comment_count, 'comments title', 'writemore' ) ),
 					number_format_i18n( $comment_count ),
 					'<span>' . get_the_title() . '</span>'
 				);
