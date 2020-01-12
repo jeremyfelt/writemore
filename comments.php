d096da8928365435eca7d1426153d8bf5f7ef821
Jeremy Felt <jeremy.felt@gmail.com>
2020-01-12 13:10:21 -0800

    Update mention markup to match like markup

--- comments.php
+++ comments.php
@@ -123,23 +123,39 @@ if ( post_password_required() ) {
 
 		<div class="webmention-mentions">
 			<h3>Mentions</h3>
-			<ul>
 		<?php
 		foreach ( $typed_comments['mention'] as $mention_id ) {
 			$mention = get_comment( $mention_id );
 			$url = get_comment_meta( $mention_id, 'webmention_source_url', true );
 			$avatar = get_comment_meta( $mention_id, 'avatar', true );
+			$author_url = get_comment_meta( $mention_id, 'semantic_linkbacks_author_url', true );
 
+			if ( '' === $author_url ) {
+				$author_url = $url;
+			}
+
+			if ( '' === $avatar ) {
+				$avatar = get_template_directory_uri() . '/images/mystery-person.png';
+			}
 			?>
-				<li>
-					<img alt="" width=50 src="<?php echo esc_url( $avatar ); ?>" />
-					<a href="<?php echo esc_url( $url ); ?>" ><?php echo esc_html( $mention->comment_author ); ?></a>
-				</li>
+
+			<article class="p-mention h-cite">
+				<!-- The loading attribute is only supported by Chrome right now, but I'd like to not use JavaScript for this. -->
+				<img src="<?php echo esc_url( $avatar ); ?>" width=40 alt="" loading="lazy" />
+
+				<!-- This span is my lazy way of enabling a vertically aligned flex display on the article element. -->
+				<span>
+					<a class="p-author h-card" href="<?php echo esc_url( $author_url ); ?>"><?php echo esc_html( $mention->comment_author ); ?></a>
+					mentioned this on
+					<a class="u-url" href="<?php echo esc_url( $url ); ?>">
+						<time class="dt-published"><?php echo get_comment_date( 'F j, Y \a\t g:i a', $mention ); ?></time>
+					</a>
+				</span>
+			</article>
 			<?php
 		}
 
 		?>
-			</ul>
 		</div>
 
 		<div class="webmention-replies">
