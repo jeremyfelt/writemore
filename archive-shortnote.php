5babf3b4444f03f75f848e3024ef2289aa075786
jeremyfelt <jeremy.felt@gmail.com>
2021-02-28 18:41:20 -0800

    Add an archive template for notes

---
+++ archive-shortnote.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * The template for displaying archive pages
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty_One
+ * @since Twenty Twenty-One 1.0
+ */
+
+get_header();
+
+$description = get_the_archive_description();
+?>
+
+<?php if ( have_posts() ) : ?>
+
+	<header class="page-header alignwide">
+		<h1 class="page-title">Notes</h1>
+		<?php if ( $description ) : ?>
+			<div class="archive-description"><?php echo wp_kses_post( wpautop( $description ) ); ?></div>
+		<?php endif; ?>
+	</header><!-- .page-header -->
+
+	<?php while ( have_posts() ) : ?>
+		<?php the_post(); ?>
+		<?php get_template_part( 'template-parts/content/content', 'single' ); ?>
+	<?php endwhile; ?>
+
+	<?php twenty_twenty_one_the_posts_navigation(); ?>
+
+<?php else : ?>
+	<?php get_template_part( 'template-parts/content/content-none' ); ?>
+<?php endif; ?>
+
+<?php get_footer(); ?>
