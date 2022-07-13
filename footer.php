133535690c65060ec35a3e0ab151d78a4d7e7250
Jeremy Felt <jeremy.felt@gmail.com>
2022-07-12 22:37:30 -0700

    Adjust the colophon

--- footer.php
+++ footer.php
@@ -12,53 +12,9 @@
 ?>
 	</main><!-- #main -->
 
-	<?php get_template_part( 'template-parts/footer/footer-widgets' ); ?>
-
 	<footer id="colophon" class="site-footer" role="contentinfo">
-
-		<?php if ( has_nav_menu( 'footer' ) ) : ?>
-			<nav aria-label="<?php esc_attr_e( 'Secondary menu', 'twentytwentyone' ); ?>" class="footer-navigation">
-				<ul class="footer-navigation-wrapper">
-					<?php
-					wp_nav_menu(
-						array(
-							'theme_location' => 'footer',
-							'items_wrap'     => '%3$s',
-							'container'      => false,
-							'depth'          => 1,
-							'link_before'    => '<span>',
-							'link_after'     => '</span>',
-							'fallback_cb'    => false,
-						)
-					);
-					?>
-				</ul><!-- .footer-navigation-wrapper -->
-			</nav><!-- .footer-navigation -->
-		<?php endif; ?>
-		<div class="site-name">
-			<?php if ( has_custom_logo() ) : ?>
-				<div class="site-logo"><?php the_custom_logo(); ?></div>
-			<?php else : ?>
-				<?php if ( get_bloginfo( 'name' ) && get_theme_mod( 'display_title_and_tagline', true ) ) : ?>
-					<?php if ( is_front_page() && ! is_paged() ) : ?>
-						<?php bloginfo( 'name' ); ?>
-					<?php else : ?>
-						<a href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php bloginfo( 'name' ); ?></a>
-					<?php endif; ?>
-				<?php endif; ?>
-			<?php endif; ?>
-		</div><!-- .site-name -->
-		<div class="powered-by">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'writemore' ) ); ?>"><?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'writemore' ), 'WordPress' );
-			?></a>
-			<span class="sep"> | </span>
-			<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s', 'writemore' ), '<a href="https://github.com/jeremyfelt/writemore">writemore</a>', '<a href="https://jeremyfelt.com">Jeremy Felt</a>' );
-			?>
-		</div><!-- .powered-by -->
+		<p>Read <a href="https://jeremyfelt.com/how-this-site-works/">how this site works</a>.</p>
+		<p>🍻</p>
 	</footer><!-- #colophon -->
 
 </div><!-- #page -->
