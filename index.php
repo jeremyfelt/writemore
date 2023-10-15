f68a2a799b2d5cff91702f58511019efc40a5e68
Jeremy Felt <jeremy.felt@gmail.com>
2023-10-15 14:14:27 -0700

    Mark index.php as unused

--- index.php
+++ index.php
@@ -1,40 +1,19 @@
 <?php
 /**
- * The main template file
+ * The default template file.
  *
- * This is the most generic template file in a WordPress theme
- * and one of the two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query.
- * E.g., it puts together the home page when no home.php file exists.
+ * I believe I've provided more specific views for the content on this site, so
+ * this template should never actually be used.
  *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ * If it is, I'm confused!
  *
  * @package WriteMore
  */
 
 get_header(); ?>
 
-<?php if ( is_home() && ! is_front_page() && ! empty( single_post_title( '', false ) ) ) : ?>
-	<header class="page-header alignwide">
-		<h1 class="page-title"><?php single_post_title(); ?></h1>
-	</header><!-- .page-header -->
-<?php endif; ?>
+Hi, how did this happen?
 
 <?php
-if ( have_posts() ) {
-
-	// Load posts loop.
-	while ( have_posts() ) {
-		the_post();
-
-		get_template_part( 'template-parts/content/card', get_post_type() );
-	}
-
-	the_posts_navigation();
-} else {
-
-	// If no content, include the "No posts found" template.
-	get_template_part( 'template-parts/content/content-none' );
-}
 
 get_footer();
