986043f8e2ff321b78a986e5c28065481e5cae77
Jeremy Felt <jeremy.felt@gmail.com>
2023-10-15 15:12:38 -0700

    Address some global variable naming

--- archive.php
+++ archive.php
@@ -7,15 +7,15 @@
 
 get_header();
 
-$description = '';
-$post_type   = get_post_type_object( get_post_type() );
+$writemore_description = '';
+$writemore_post_type   = get_post_type_object( get_post_type() );
 
 if ( 'shortnote' === get_post_type() ) {
-	$description = 'For shorter content, like notes.';
+	$writemore_description = 'For shorter content, like notes.';
 } elseif ( 'like' === get_post_type() ) {
-	$description = 'A list of pages I\'ve found interesting.';
+	$writemore_description = 'A list of pages I\'ve found interesting.';
 } elseif ( 'post' === get_post_type() ) {
-	$description = 'For things with titles.';
+	$writemore_description = 'For things with titles.';
 }
 
 ?>
@@ -23,9 +23,9 @@ if ( 'shortnote' === get_post_type() ) {
 <?php if ( have_posts() ) : ?>
 
 	<header>
-		<h1><?php echo esc_html( $post_type->label ); ?></h1>
-		<?php if ( ! empty( trim( $description ) ) ) : ?>
-		<p><?php echo wp_kses_post( $description ); ?></p>
+		<h1><?php echo esc_html( $writemore_post_type->label ); ?></h1>
+		<?php if ( ! empty( trim( $writemore_description ) ) ) : ?>
+		<p><?php echo wp_kses_post( $writemore_description ); ?></p>
 		<?php endif; ?>
 		<p><a href="<?php echo get_post_type_archive_feed_link( get_post_type() ); ?>">Follow via RSS</a></p>
 	</header><!-- .page-header -->
@@ -38,8 +38,8 @@ if ( 'shortnote' === get_post_type() ) {
 	<?php
 	the_posts_navigation(
 		array(
-			'prev_text' => 'View older ' . strtolower( $post_type->labels->name ),
-			'next_text' => 'View newer ' . strtolower( $post_type->labels->name ),
+			'prev_text' => 'View older ' . strtolower( $writemore_post_type->labels->name ),
+			'next_text' => 'View newer ' . strtolower( $writemore_post_type->labels->name ),
 		)
 	);
 
