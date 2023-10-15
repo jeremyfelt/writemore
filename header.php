4248ceeb6e5a172ed36294a7bf87fcf5dfd993c4
Jeremy Felt <jeremy.felt@gmail.com>
2023-10-15 14:21:15 -0700

    Consolidate the header template

--- header.php
+++ header.php
@@ -1,14 +1,11 @@
 <?php
 /**
- * The header.
- *
- * This is the template that displays all of the <head> section and everything up until main.
- *
- * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ * The header template.
  *
  * @package WriteMore
  */
 
+$writemore_blog_info = get_bloginfo( 'name' );
 ?>
 <!doctype html>
 <html <?php language_attributes(); ?>>
@@ -22,6 +19,35 @@
 <?php wp_body_open(); ?>
 <a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'writemore' ); ?></a>
 
-<?php get_template_part( 'template-parts/header/site-header' ); ?>
+<header>
+
+	<div class="site-branding">
+		<?php if ( is_front_page() && ! is_paged() ) : ?>
+			<h1><?php echo esc_html( $writemore_blog_info ); ?></h1>
+		<?php elseif ( is_front_page() && ! is_home() ) : ?>
+			<h1><a href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php echo esc_html( $writemore_blog_info ); ?></a></h1>
+		<?php else : ?>
+			<p><?php echo esc_html( $writemore_blog_info ); ?></p>
+		<?php endif; ?>
+	</div>
+
+	<?php if ( has_nav_menu( 'header-menu' ) ) : ?>
+	<nav id="site-navigation" aria-label="<?php esc_attr_e( 'Primary menu', 'writemore' ); ?>">
+		<?php
+		wp_nav_menu(
+			array(
+				'theme_location'  => 'primary',
+				'menu_class'      => 'menu-wrapper',
+				'container'       => false,
+				'container_class' => 'primary-menu-container',
+				'items_wrap'      => '<ul id="primary-menu-list">%3$s</ul>',
+				'fallback_cb'     => false,
+			)
+		);
+		?>
+	</nav>
+	<?php endif; ?>
+
+</header>
 
 <main id="main">
