1205cbbe413f6bde9be2ad941c04dd960079e500
Jeremy Felt <jeremy.felt@gmail.com>
2023-10-15 14:35:47 -0700

    Clean up page card further

--- template-parts/content/card-page.php
+++ template-parts/content/card-page.php
@@ -9,24 +9,12 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<?php
-	if ( ! is_front_page() && post_type_supports( get_post_type(), 'title' ) ) :
-		?>
-	<header>
-		<?php
-		if ( is_singular() ) {
-			the_title( '<h1 class="p-name">', '</h1>' );
-		} else {
-			the_title( '<h1><a href="' . get_the_permalink() . '" class="p-name">', '</a></h1>' );
-		}
-
-		?>
-		<?php the_post_thumbnail(); ?>
-	</header>
-		<?php
-	endif;
-
 	if ( ! is_front_page() ) {
 		?>
+		<header>
+			<?php the_title( '<h1 class="p-name">', '</h1>' ); ?>
+			<?php the_post_thumbnail(); ?>
+		</header>
 		<div class="entry-content e-content">
 			<?php the_content(); ?>
 		</div><!-- .entry-content -->
@@ -39,12 +27,5 @@
 	<footer class="entry-footer">
 		<?php get_template_part( 'template-parts/post/author-bio' ); ?>
 	</footer><!-- .entry-footer -->r
-
-	<?php
-	// If comments are open or there is at least one comment, load up the comment template.
-	if ( comments_open() || get_comments_number() ) {
-		comments_template(); // Outputs its own <section>.
-	}
-	?>
 
 </article><!-- #post-<?php the_ID(); ?> -->
