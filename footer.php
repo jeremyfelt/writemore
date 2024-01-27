763ff3c7c2cf0ed047684f41161f88bcd2f20b89
Jeremy Felt <jeremy.felt@gmail.com>
2024-01-27 14:51:42 -0800

    Improve footer layout
    Fixes #37

--- footer.php
+++ footer.php
@@ -9,8 +9,10 @@
 	</main><!-- #main -->
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
-		<p><a href="https://xn--sr8hvo.ws/previous">←</a> An <a href="https://xn--sr8hvo.ws">IndieWeb Webring</a> 🕸💍 <a href="https://xn--sr8hvo.ws/next">→</a></p>
-		<p>Powered by open source. Read <a href="https://jeremyfelt.com/how-this-site-works/">how this site works</a>. 🍻</p>
+		<section>
+			<p><a href="https://xn--sr8hvo.ws/previous">←</a> An <a href="https://xn--sr8hvo.ws">IndieWeb Webring</a> 🕸💍 <a href="https://xn--sr8hvo.ws/next">→</a></p>
+			<p>Powered by open source. Read <a href="https://jeremyfelt.com/how-this-site-works/">how this site works</a>. 🍻</p>
+		</section>
 	</footer><!-- #colophon -->
 
 </div><!-- #page -->
